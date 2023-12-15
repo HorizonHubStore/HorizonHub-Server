849552b9d2260afa4179c979971e591dc9a28c10
MrGlam <elonglam@gmail.com>
2023-12-15 15:28:03 +0200

    Add authentication middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const accessTokenSecret: string = process.env.ACCESS_TOKEN_SECRET as string;
+
+const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authHeaders = req.headers['authorization'];
+        const token = authHeaders && authHeaders.split(' ')[1];
+
+        if (token == null) {
+            return res.sendStatus(401); // Unauthorized
+        }
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.status(403).send(err.message);
+            }
+
+            // @ts-ignore
+            req.user = user;
+            next();
+        });
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(500); // Internal Server Error
+    }
+};
+
+export default authenticate;
