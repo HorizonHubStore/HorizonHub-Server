588cb8a15bc00f13d030b62cbeb90a7c53776553
MrGlam <elonglam@gmail.com>
2023-12-23 22:19:13 +0200

    add post routing

---
+++ src/routes/postRoutes.ts
@@ -0,0 +1,31 @@
+// routes.ts
+import express from "express";
+import authenticate from '../middleware/auth';
+import * as postController from "../controllers/postController";
+import multer from 'multer'
+import path from 'path'
+
+const router = express.Router();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      // Specify the destination folder for your uploads
+      cb(null, 'public/images/');
+    },
+    filename: function (req, file, cb) {
+      // Specify the filename for the uploaded file
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+const upload = multer({ storage: storage });
+// Post routes
+router.post("/createPost", authenticate, upload.fields([
+    { name: 'picture', maxCount: 1 },
+    { name: 'gameFile', maxCount: 1 }
+  ]),postController.createPost);
+
+router.get("/getAllPosts",postController.getAllPosts)
+router.put("/posts/:id", authenticate, postController.updatePost); 
+router.delete("/posts/:id", authenticate, postController.deletePost); 
+
+export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,23 @@
 // userRoutes.js
 const express = require("express");
 const router = express.Router();
-import * as userController from "../controllers/userController";
+import multer from 'multer'
+import path from 'path'
 import * as FileController from "../controllers/fileController";
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      // Specify the destination folder for your uploads
+      cb(null, 'public/images/');
+    },
+    filename: function (req, file, cb) {
+      // Specify the filename for the uploaded file
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+const upload = multer({ storage: storage });
 // Define routes
-router.put(
-    "/updatePicture",
-    FileController.uploadPicture,
-);
+router.put("/updatePicture",FileController.uploadPicture);
 
 export default router;
